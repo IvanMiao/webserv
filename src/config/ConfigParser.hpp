e4ed770362b37048d526a3635857fbfc5abaa7b4
Yifan Miao <ymiao@f6r6s13.clusters.42paris.fr>
2025-12-06 18:13:58 +0100

    prepare config class

--- src/config/ConfigParser.hpp
+++ src/config/ConfigParser.hpp
@@ -1,5 +1,56 @@
 #ifndef CONFIG_PARSER_HPP
 #define CONFIG_PARSER_HPP
 
+#include <string>
+#include <map>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
+class LocationConfig
+{
+public:
+	std::string		path;
+	std::string		root;
+	std::vector<std::string>	allow_methods;
+	std::string		index;
+	bool			autoindex;
+
+public:
+	LocationConfig();
+};
+
+
+class ServerConfig
+{
+public:
+	int			listen_port;
+	std::string	server_name;
+	std::string	root;
+	size_t		client_max_body_size;
+	// ... add more params
+	std::map<int, std::string>	error_pages;
+	std::vector<LocationConfig>	locations;
+
+public:
+	ServerConfig();
+};
+
+
+class ConfigParser
+{
+private:
+	std::string					_filepath;
+	std::vector<ServerConfig>	_servers;
+
+public:
+	ConfigParser(const std::string& file_path);
+	~ConfigParser();
+
+	void parse();
+	const std::vector<ServerConfig>& getServers() const;
+};
 
 #endif
